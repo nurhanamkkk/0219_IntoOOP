8b365fa138ffcf0080ad3c9f90dce1ded6dcf03b
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-02 14:53:54 +0700

    mengubah kata nmamen menjadi nama

--- pertemuan keenam/4.cpp
+++ pertemuan keenam/4.cpp
@@ -27,6 +27,11 @@ int main () {
         cin.ignore (1, '\n');
         cout << endl;
     }
-        
+       for (int i = 0; i < 3; i++)  {
+        cout << endl;
+        cout << "data mahasiswa ke-" << (i=1) << ":" << endl;
+        cout << "\n nim : " << mhs[i].nama;
+        cout << "\n nmamen"
+       } 
     }
 }
