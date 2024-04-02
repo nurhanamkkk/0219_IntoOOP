cd951d1c5b6d32872e7fee7fe28adfd85f961587
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-02 13:59:18 +0700

    membuat variabel mhs

--- pertemuan keenam/struct DetailAlamat2.cpp
+++ pertemuan keenam/struct DetailAlamat2.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 using namespace std;
 
-struct DetailAlamat {}
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+struct mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+int main () {
+    mahasiswa mhs; 
+    cout << "nomor mahasiswa : "; getline (cin, mhs.nim);
+    cout << "nama mahasiswa : "; getline (cin, mhs.nama);
+
+    cout << "alamat mahasiswa : " <<endl;
+    cout << "\t nama desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout <<endl;
+}
+    
