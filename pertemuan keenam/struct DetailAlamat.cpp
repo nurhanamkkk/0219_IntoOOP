84a50301e4afc7f38af26df8ab0612b184065215
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-02 13:43:40 +0700

    membuat struct detail Alamat

--- pertemuan keenam/struct DetailAlamat.cpp
+++ pertemuan keenam/struct DetailAlamat.cpp
@@ -6,4 +6,30 @@ struct DetailAlamat {
     string kota;
 };
 
-struct mahasiswa {}
+struct mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main (){
+    mahasiswa mhs;
+    cout << "nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "alamat mahasiswa : ";
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n alamat : ";
+    cout << "\n desa : " << mhs.alamat.desa;
+    cout << "\n kota : " << mhs.alamat.kota;
+    cout << "\n umur : " << mhs.umur;
+};
