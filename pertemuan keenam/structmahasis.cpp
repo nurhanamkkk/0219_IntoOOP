834ce3cfa7aeb32bb81c57a40be8433fe7370d75
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-02 13:26:38 +0700

    membuat struct mahasiswa

--- pertemuan keenam/structmahasis.cpp
+++ pertemuan keenam/structmahasis.cpp
@@ -12,9 +12,18 @@ struct mahasiswa {
 int main(){
     mahasiswa mhs;
     cout << "Nomor Mahasiswa : ";
-    cin >> mhs.nim;
+    cin >> mhs.nim;     
     cout << "nama mahasiswa : ";
     cin >> mhs.nama;
-    cout << ""
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim :" << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
 
 };
