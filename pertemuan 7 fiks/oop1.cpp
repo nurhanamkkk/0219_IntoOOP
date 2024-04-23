0a34292d91ae23361f14313b4811726f75a79841
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-23 16:26:00 +0700

    memasukkan matakuliah

--- pertemuan 7 fiks/oop1.cpp
+++ pertemuan 7 fiks/oop1.cpp
@@ -7,7 +7,7 @@ class Mahasiswa{
          int umur;
          string jurusan;
 
-         voide output () {
+         void output () {
             cout<< "Nama :" << nama << endl;
             cout << "Umur: " << umur << endl;
             cout << "Jurusan : " << jurusan << endl;
