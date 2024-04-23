c9e6857c84fe16c0d4040d69a5cb41ed01fb4985
nurhanamkkk <nurhanamk.30@gmail.com>
2024-04-23 16:20:09 +0700

    mengubah kata Lebaernya menjadi lebarnya

--- pertemuan 7 fiks/oop2.cpp
+++ pertemuan 7 fiks/oop2.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 
 class bangunDatar{
+     
+     //akses modifier
+     private:
+         float panjang, Lebar;
+     public:
+          float Luas;
 
-    
+          void input() {  //methode input persegi panjang 
+           
+           cout << "Masukkan panjangnya = ";
+           cin >> panjang;
+           cout << "Masukkan Lebaernya = ";
+           cin >> Lebar;
+          }   
+
+          float hitungluas (){
+              return panjang * Lebar;
+          } 
+
+          void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << Lebar << endl;
+            cout << "Luasnya = " << Luas << endl;
+          }
+};
+
+int main () {
+    bangunDatar pp; // dekalarasi objek pp dari bangunDatar
+    pp.input();
+    pp.display();
+
+    return 0;
 }
